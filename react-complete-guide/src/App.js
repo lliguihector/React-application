61762683118264a48966420aa972ecab965c2485
Hector Lliguichuzhca <lliguichuzcah@gmail.com>
2019-12-18 14:16:21 -0500

    added dynamic functionality, created a new container js and css file, Render content Conditionaly  lesson 53

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+
+//import persons component dont need to add the .js extencion 
 import Person from './Person/Person';
 
+
+//CLASS 
 class App extends Component {
+
+
+  //State 
 state = {
 persons: [
   {name: 'Max',age: 28},
   {name: 'Manu', age: 29},
   {name: 'Stephanie', age: 26}
-]
+],
+otherState: 'some other value',
+showPersons: false 
 }
 
-//ES6 Arrow function 
+//ES6 Arrow function  changes the first name of the array list 
 switchNameHandler = (newName) => {
 //console.log('was clicked!');
 
@@ -23,11 +32,10 @@ this.setState({
     {name: 'Manu', age: 29},
     {name: 'Stephanie', age: 27}
   ]
-
-
 })
 }
 
+//Function 
 nameChangedHandler = (event) =>{
   this.setState({
     persons:[
@@ -40,23 +48,35 @@ nameChangedHandler = (event) =>{
   })
 }
 
+
+//Function 
+togglePersonsHandler = () => {
+const doesShow = this.state.showPersons;
+this.setState({showPersons: !doesShow});
+}
+
+
   //rendor method
   render() {
-
+    //Inline style only use to scope your style in the desire target component 
 const style = {
   backgroundColor: 'white',
   font: 'inherit',
   border: '1px solid blue',
   padding: '8px',
-  cursor:'pointer'
+  cursor:'pointer',
+  background: 'green',
+  color: 'white'
 };
 
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button style={style}
-        onClick={this.switchNameHandler.bind(this,'James')}>Switch Name</button>
+        <button style={style} onClick={this.togglePersonsHandler}>Toggle Person</button>
+
+        {  this.state.showPersons ? 
+        <div>
         <Person 
         name={this.state.persons[0].name} 
         age={this.state.persons[0].age}>My Hobbies: Racing</Person>
@@ -68,6 +88,8 @@ const style = {
         <Person 
         name={this.state.persons[2].name} 
         age={this.state.persons[2].age}/>
+        </div> : null
+        }
       </div>
     );
   }
